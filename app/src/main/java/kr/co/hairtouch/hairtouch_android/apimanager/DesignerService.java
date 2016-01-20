555cc59a00e107f1bccb91adf0d6b317e166d736
tjlee@woowahan.com <tjlee@woowahan.com>
2016-01-20 23:31:06 +0900

    Designer 조회화면 추가

---
+++ app/src/main/java/kr/co/hairtouch/hairtouch_android/apimanager/DesignerService.java
@@ -0,0 +1,19 @@
+package kr.co.hairtouch.hairtouch_android.apimanager;
+
+import java.util.List;
+
+import kr.co.hairtouch.hairtouch_android.model.Designer;
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * Created by leetaejun on 2016. 1. 20..
+ */
+public interface DesignerService {
+    @GET("designers.json")
+    Call<List<Designer>> loadDesigners();
+
+    @GET("designers/{id}.json")
+    Call<Designer> loadDesigner(@Path("id") int id);
+}
